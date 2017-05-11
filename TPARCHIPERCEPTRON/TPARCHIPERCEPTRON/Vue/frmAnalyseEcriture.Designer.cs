7aa59158e72e5ab77c9a3a23545f4d8b6a2680a0
Renaud Gariepy <renaud.gariepy@gmail.com>
2017-05-11 13:45:59 -0400

    traduction

--- TPARCHIPERCEPTRON/TPARCHIPERCEPTRON/Vue/frmAnalyseEcriture.Designer.cs
+++ TPARCHIPERCEPTRON/TPARCHIPERCEPTRON/Vue/frmAnalyseEcriture.Designer.cs
@@ -28,59 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ucPremierControle1 = new TPARCHIPERCEPTRON.Vue.ucPremierControle();
-            this.ucDeuxiemeControle1 = new TPARCHIPERCEPTRON.Vue.ucDeuxiemeControle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAnalyseEcriture));
             this.txtConsole = new System.Windows.Forms.TextBox();
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.tsmiLangue = new System.Windows.Forms.ToolStripMenuItem();
+            this.ucDeuxiemeControle1 = new TPARCHIPERCEPTRON.Vue.ucDeuxiemeControle();
+            this.ucPremierControle1 = new TPARCHIPERCEPTRON.Vue.ucPremierControle();
+            this.menuPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtConsole
+            // 
+            resources.ApplyResources(this.txtConsole, "txtConsole");
+            this.txtConsole.Name = "txtConsole";
+            // 
+            // menuPrincipal
+            // 
+            resources.ApplyResources(this.menuPrincipal, "menuPrincipal");
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiLangue});
+            this.menuPrincipal.Name = "menuPrincipal";
+            // 
+            // tsmiLangue
+            // 
+            resources.ApplyResources(this.tsmiLangue, "tsmiLangue");
+            this.tsmiLangue.Name = "tsmiLangue";
+            this.tsmiLangue.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.langueToolStripMenuItem_DropDownItemClicked);
+            // 
+            // ucDeuxiemeControle1
+            // 
+            resources.ApplyResources(this.ucDeuxiemeControle1, "ucDeuxiemeControle1");
+            this.ucDeuxiemeControle1.CstApprentissage = 0D;
+            this.ucDeuxiemeControle1.FichierEntrainement = null;
+            this.ucDeuxiemeControle1.ModePhrase = false;
+            this.ucDeuxiemeControle1.Name = "ucDeuxiemeControle1";
+            this.ucDeuxiemeControle1.ResultText = "";
+            this.ucDeuxiemeControle1.TextGrpDessin = "Zone de dessin";
+            this.ucDeuxiemeControle1.BoutonOKClick += new TPARCHIPERCEPTRON.Vue.ucDeuxiemeControle.OKButtonClickHandler(this.ucDeuxiemeControle1_BoutonOKClick);
             // 
             // ucPremierControle1
             // 
+            resources.ApplyResources(this.ucPremierControle1, "ucPremierControle1");
             this.ucPremierControle1.ConstanteApprentissage = 0D;
             this.ucPremierControle1.EmplacementFichierEntrainement = null;
-            this.ucPremierControle1.Location = new System.Drawing.Point(12, 12);
             this.ucPremierControle1.Name = "ucPremierControle1";
             this.ucPremierControle1.NomFichierEntrainement = null;
             this.ucPremierControle1.NouveauFichier = false;
-            this.ucPremierControle1.Size = new System.Drawing.Size(526, 192);
-            this.ucPremierControle1.TabIndex = 0;
+            this.ucPremierControle1.ResultatEntrainement = "";
             this.ucPremierControle1.Titre = "Entrainement";
             this.ucPremierControle1.BoutonEffacerClick += new TPARCHIPERCEPTRON.Vue.ucPremierControle.EffacerButtonClickHandler(this.ucPremierControle1_BoutonEffacerClick);
             this.ucPremierControle1.BoutonEntrainerClick += new TPARCHIPERCEPTRON.Vue.ucPremierControle.EntrainerButtonClickHandler(this.ucPremierControle1_BoutonEntrainerClick);
             // 
-            // ucDeuxiemeControle1
-            // 
-            this.ucDeuxiemeControle1.CstApprentissage = 0D;
-            this.ucDeuxiemeControle1.FichierEntrainement = null;
-            this.ucDeuxiemeControle1.Location = new System.Drawing.Point(94, 210);
-            this.ucDeuxiemeControle1.ModePhrase = false;
-            this.ucDeuxiemeControle1.Name = "ucDeuxiemeControle1";
-            this.ucDeuxiemeControle1.ResultText = "";
-            this.ucDeuxiemeControle1.Size = new System.Drawing.Size(357, 331);
-            this.ucDeuxiemeControle1.TabIndex = 1;
-            this.ucDeuxiemeControle1.TextGrpDessin = "Zone de dessin";
-            this.ucDeuxiemeControle1.BoutonOKClick += new TPARCHIPERCEPTRON.Vue.ucDeuxiemeControle.OKButtonClickHandler(this.ucDeuxiemeControle1_BoutonOKClick);
-            // 
-            // txtConsole
-            // 
-            this.txtConsole.Location = new System.Drawing.Point(40, 547);
-            this.txtConsole.Multiline = true;
-            this.txtConsole.Name = "txtConsole";
-            this.txtConsole.Size = new System.Drawing.Size(472, 181);
-            this.txtConsole.TabIndex = 2;
-            // 
             // frmAnalyseEcriture
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 740);
             this.Controls.Add(this.txtConsole);
             this.Controls.Add(this.ucDeuxiemeControle1);
             this.Controls.Add(this.ucPremierControle1);
+            this.Controls.Add(this.menuPrincipal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuPrincipal;
             this.MaximizeBox = false;
             this.Name = "frmAnalyseEcriture";
-            this.Text = "Analyse Écriture";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmAnalyseEcriture_FormClosing);
+            this.Load += new System.EventHandler(this.frmAnalyseEcriture_Load);
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +105,8 @@
         private Vue.ucPremierControle ucPremierControle1;
         private Vue.ucDeuxiemeControle ucDeuxiemeControle1;
         private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem tsmiLangue;
     }
 }
 
